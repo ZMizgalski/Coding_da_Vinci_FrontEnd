b12c35eaadf21164e2f2b4d66e1a3661a2d6b4f1
KrzysztofProgramming <krzychuk112@gmail.com>
2022-03-20 13:36:44 +0100

    nawigacja i zarys strony tytulowej

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
+import { NotFoundComponent } from './router-components/not-found/not-found.component';
+import { AboutComponent } from './router-components/about/about.component';
+import { MixerComponent } from './router-components/mixer/mixer.component';
+import { CollectionComponent } from './router-components/collection/collection.component';
+import { HomeComponent } from './router-components/home/home.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "home", component: HomeComponent},
+  {path: "collection", component: CollectionComponent},
+  {path: "mixer", component: MixerComponent},
+  {path: "about", component: AboutComponent},
+  {path: "", pathMatch: 'full', redirectTo: "home"},
+  {path: "**", component: NotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,24 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MainNavComponent } from './router-components/main-nav/main-nav.component';
 import { TopNavComponent } from './utils-components/top-nav/top-nav.component';
+import { HomeComponent } from './router-components/home/home.component';
+import { CollectionComponent } from './router-components/collection/collection.component';
+import { AboutComponent } from './router-components/about/about.component';
+import { MixerComponent } from './router-components/mixer/mixer.component';
+import { NotFoundComponent } from './router-components/not-found/not-found.component';
+import { ImageBrowserComponent } from './utils-components/image-browser/image-browser.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MainNavComponent,
-    TopNavComponent
+    TopNavComponent,
+    HomeComponent,
+    CollectionComponent,
+    AboutComponent,
+    MixerComponent,
+    NotFoundComponent,
+    ImageBrowserComponent
   ],
   imports: [
     BrowserModule,
