9e3a2a6f822f5805e51fb380bb2ced21312ec4ca
ZMizgalski <zbigniew.mizgalskifullstack@gmail.com>
2022-04-14 19:18:49 +0200

    update

--- src/app/router-components/main-nav/main-nav.component.ts
+++ src/app/router-components/main-nav/main-nav.component.ts
@@ -3,12 +3,34 @@ import { Subscription } from 'rxjs';
 import { ResizeService } from './../../services/resize-handler/resize.service';
 import { NavItemModel } from '../../utils-components/side-nav/side-nav.component';
 import { Component, OnInit, OnDestroy, ChangeDetectionStrategy } from '@angular/core';
+import { animate, animation, style, transition, trigger, useAnimation } from '@angular/animations';
+
 
 @Component({
   selector: 'app-main-nav',
   templateUrl: './main-nav.component.html',
   styleUrls: ['./main-nav.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
+  animations: [
+    trigger('ease', [
+      transition(':enter', [
+        style({opacity:0}),
+        animate(100, style({opacity:1})) 
+      ]),
+      transition(':leave', [
+        animate(100, style({opacity:0})) 
+      ])
+    ]),
+    trigger('ease2', [
+      transition(':enter', [
+        style({opacity:0}),
+        animate(1000, style({opacity:1})) 
+      ]),
+      transition(':leave', [
+        animate(100, style({opacity:0})) 
+      ])
+    ])
+  ]
 })
 export class MainNavComponent implements OnInit, OnDestroy {
   public navExpand = false;
